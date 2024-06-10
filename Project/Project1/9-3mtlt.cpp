6ace047551c44ddf8c162fbadd0306e3c4a20660
panda <2414979240@qq.com>
2024-06-10 17:01:24 +0800

    增加了Linux远程VS2019项目和相关中文注释

---
+++ Project/Project1/9-3mtlt.cpp
@@ -0,0 +1,177 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+#define MAX_EVENT_NUMBER 1024
+#define BUFFER_SIZE 10
+
+int setnonblocking( int fd )
+{
+    int old_option = fcntl( fd, F_GETFL );
+    int new_option = old_option | O_NONBLOCK;
+    fcntl( fd, F_SETFL, new_option );
+    return old_option;
+}
+
+void addfd( int epollfd, int fd, bool enable_et )
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    if( enable_et )
+    {
+        event.events |= EPOLLET;
+    }
+    epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &event );
+    setnonblocking( fd );
+}
+
+void lt( epoll_event* events, int number, int epollfd, int listenfd )
+{
+    char buf[ BUFFER_SIZE ];
+    for ( int i = 0; i < number; i++ )
+    {
+        int sockfd = events[i].data.fd;
+        if ( sockfd == listenfd )
+        {
+            struct sockaddr_in client_address;
+            socklen_t client_addrlength = sizeof( client_address );
+            int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
+            addfd( epollfd, connfd, false );
+        }
+        //EPOLLIN监听数据是否可读
+        else if ( events[i].events & EPOLLIN )
+        {
+            printf( "event trigger once\n" );
+            memset( buf, '\0', BUFFER_SIZE );
+            int ret = recv( sockfd, buf, BUFFER_SIZE-1, 0 );
+            if( ret <= 0 )
+            {
+                close( sockfd );
+                continue;
+            }
+            printf( "get %d bytes of content: %s\n", ret, buf );
+        }
+        else
+        {
+            printf( "something else happened \n" );
+        }
+    }
+}
+
+/*  在ET模式下，A作为客户端链接到服务器，首先我们会将A.socketfd注册到单独的epoll_event中，然后在epoll_event中设置监听fd为事件可读（EPOLLIN）
+并设置当前epoll_event事件为边沿触发模式（ET：表示只会触发一次），上传到内核时间表，然后将A.socketfd设置为非阻塞状态。
+    然后循环等待fd事件可读。
+    当fd事件可读，由于ET模式只会触发一次，由于fd处于非阻塞状态，我们需要循环读取（recv) A.fd上面的可读数据。直到没有数据可读
+    recv > 0 :表示收到的实际数据长度
+    recv== 0 :表示对方可能关闭链接
+    recv==-1 :表示出错，并设置error
+*/
+void et( epoll_event* events, int number, int epollfd, int listenfd )
+{
+    char buf[ BUFFER_SIZE ];
+    for ( int i = 0; i < number; i++ )
+    {
+        int sockfd = events[i].data.fd;
+        if ( sockfd == listenfd )
+        {
+            struct sockaddr_in client_address;
+            socklen_t client_addrlength = sizeof( client_address );
+            int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
+            addfd( epollfd, connfd, true );
+        }
+        else if ( events[i].events & EPOLLIN )
+        {
+            printf( "event trigger once\n" );
+            while( 1 )
+            {
+                memset( buf, '\0', BUFFER_SIZE );
+                int ret = recv( sockfd, buf, BUFFER_SIZE-1, 0 );
+                if( ret < 0 )
+                {
+                    /*
+                    对于recv返回小于0 为什么会有这样的操作呢？
+                        原来对于‘非阻塞’IO，下面的条件成立，表示数据已经完全读取完毕，以后epool就能继续触发A.fd上的可读事件，以便驱动下次操作
+                    */
+                    if( ( errno == EAGAIN ) || ( errno == EWOULDBLOCK ) )
+                    {
+                        printf( "read later\n" );
+                        break;
+                    }
+                    close( sockfd );
+                    break;
+                }
+                else if( ret == 0 )
+                {
+                    close( sockfd );
+                }
+                else
+                {
+                    printf( "get %d bytes of content: %s\n", ret, buf );
+                }
+            }
+        }
+        else
+        {
+            printf( "something else happened \n" );
+        }
+    }
+}
+
+int main( int argc, char* argv[] )
+{
+    if( argc <= 2 )
+    {
+        printf( "usage: %s ip_address port_number\n", basename( argv[0] ) );
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi( argv[2] );
+
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ) );
+    address.sin_family = AF_INET;
+    inet_pton( AF_INET, ip, &address.sin_addr );
+    address.sin_port = htons( port );
+
+    int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+    assert( listenfd >= 0 );
+
+    ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
+    assert( ret != -1 );
+
+    ret = listen( listenfd, 5 );
+    assert( ret != -1 );
+
+    epoll_event events[ MAX_EVENT_NUMBER ];
+    int epollfd = epoll_create( 5 );
+    assert( epollfd != -1 );
+    addfd( epollfd, listenfd, true );
+
+    while( 1 )
+    {
+        int ret = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
+        if ( ret < 0 )
+        {
+            printf( "epoll failure\n" );
+            break;
+        }
+    
+        lt( events, ret, epollfd, listenfd );
+        //et( events, ret, epollfd, listenfd );
+    }
+
+    close( listenfd );
+    return 0;
+}
